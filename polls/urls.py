a0b93aa07b6edbbf197a8bc4c66b5b77374d0a6e
Your Name <you@example.com>
2020-08-26 20:03:48 +0900

    updated pollsapi 8/26

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .apiviews import PollViewSet, ChoiceList, CreateVote, UserCreate
+from .apiviews import ( PollViewSet, 
+                        ChoiceList,
+                        CreateVote, 
+                        UserCreate, 
+                        LoginView
+)
+from rest_framework.authtoken import views
 
 urlpatterns = [
+    path("login/", views.obtain_auth_token, name="login"),
+    path("login/", LoginView.as_view(), name="login"),
     path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
     path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
     path("users/", UserCreate.as_view(), name="user_create"),
